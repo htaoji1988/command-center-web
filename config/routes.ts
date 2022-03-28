12d6443ba69f31f7b178c85c8b261e73b5359847
TT-YOGA14S\ht <htaoji1988@126.com>
2022-03-28 23:17:01 +0800

    no message

---
+++ config/routes.ts
@@ -0,0 +1,74 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    name: 'userPermission',
+    icon: 'UserOutlined',
+    path: '/user_permission',
+    routes: [
+      {
+        path: '/user_permission/usermanage',
+        name: 'userlist',
+        icon: 'smile',
+        component: './UserManage',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
